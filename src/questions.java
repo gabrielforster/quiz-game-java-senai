69b6657b09d14dfd5ea860619757975236a229bb
gabrielforster <rochafrgabriel@gmail.com>
2022-06-21 22:10:53 -0300

    feat: implementing answer list

--- src/questions.java
+++ src/questions.java
@@ -1,11 +1,29 @@
-import java.util.ArrayList;
-
 public class questions {
 
-        public static String[] questionsList = new String[5];
-        public static void getQuestionsList(){
-            questionsList[0] = "Qual a sua idade?";
-
-        };
-
+    public static String[] questionsList = new String[10];
+    public static void getQuestionsList(){
+        questionsList[0] = "Qual a sua idade?";
+        questionsList[1] = "100 + 100?";
+        questionsList[2] = "Pergunta teste?";
+        questionsList[3] = "";
+        questionsList[4] = "";
+        questionsList[5] = "";
+        questionsList[6] = "";
+        questionsList[7] = "";
+        questionsList[8] = "";
+        questionsList[9] = "";
+    };
+    public static String[] answersList = new String[10];
+    public static void getAnswersList() {
+    	answersList[0] = "17";
+    	answersList[1] = "200";
+    	answersList[2] = "Sim";
+    	answersList[3] = "";
+    	answersList[4] = "";
+    	answersList[5] = "";
+    	answersList[6] = "";
+    	answersList[7] = "";
+    	answersList[8] = "";
+    	answersList[9] = "";
     }
+}
--- src/quiz.java
+++ src/quiz.java
@@ -1,20 +1,20 @@
-import java.util.ArrayList;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class quiz {
-    private static String nonGurreiro (){
+    
+	private static String nonGurreiro (){
         System.out.println("Gurreiro nao existente");
         return "";
     }
+    
     public static void main(String[] args) {
         questions.getQuestionsList();
+        questions.getAnswersList();
         Scanner sc = new Scanner(System.in);
 
         String guerreiro = "";
         int fighterLife = 6, monsterLife = 6;
         System.out.println(questions.questionsList[0]);
-
 
         //Definido gurreiros disponiveis no game
         String espada =
@@ -29,7 +29,7 @@ public class quiz {
                 "   |  \n" +
                 "  / \\";
         System.out.println("Selecione entre um dos guerreiros (Lembre-se que quanto maior a ferramenta de ataque, mais lento o ataque especial!)");
-        System.out.println("Espada: \n" + espada + "\nFaca:\n" + faca + "\nEscolha entre guerreiro com espada ou faca?");
+        System.out.println("Guerreiro com Espada: \n" + espada + "\n\nGuerreiro com Faca:\n" + faca + "\nEscolha entre guerreiro com espada ou faca?");
         String answer = sc.next().toLowerCase();
 
         guerreiro = answer.equals("faca") ? faca : answer.equals("espada") ? espada : nonGurreiro();
@@ -41,12 +41,37 @@ public class quiz {
         else if(guerreiro.equals(espada)){
             System.out.println("Seu ataque especial carrega em TRÊS respostas certas!");
         }
-        do{
-//            double questionNumber = Math.ceil(Math.random()* 10);
-            double questionNumber =  1;
+        
+        while(fighterLife > 0 && monsterLife > 0) {
+//            double questionNumber = Math.ceil(Math.random()* 10) - 1;
+        	double questionNumber = 2;
+            
             String actualQuestion = questions.questionsList[(int)questionNumber];
-            System.out.println(actualQuestion);
-        }while(fighterLife != 0 && monsterLife != 0);
+            System.out.println(actualQuestion+"\n"
+            		+ "-Resposta-> ");
+            String questionAnswer = sc.nextLine();
+            
+//            fighterLife += questionAnswer.toLowerCase().equals(questions.answersList[(int)questionNumber].toLowerCase()) ? 1 : -1;
+            
+            if (questionAnswer.toLowerCase().equals(questions.answersList[(int)questionNumber].toLowerCase())) {
+            	fighterLife += 1;
+            	monsterLife -= 1;
+            }
+            if(!questionAnswer.toLowerCase().equals(questions.answersList[(int)questionNumber].toLowerCase())){            	
+            	monsterLife += 1;
+            	fighterLife -= 1;
+            }
+            
+            if(fighterLife == 0 || monsterLife==0) {
+            	break;
+            }
+        }
+            
+        
+        if(fighterLife <= 0) System.out.println("Guerreiro morto!");
+
+        if(monsterLife <= 0) System.out.println("Monstro morto!");
+        
         sc.close();
     }
 }
