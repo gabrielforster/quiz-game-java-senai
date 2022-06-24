59a5677c497fe157547256f3b639772612047781
gabrielforster <rochafrgabriel@gmail.com>
2022-06-24 08:48:15 -0300

    Finish

--- src/questions.java
+++ src/questions.java
@@ -4,14 +4,14 @@ public class questions {
     public static void getQuestionsList(){
         questionsList[0] = "Qual era a forma de governo em Atenas?";
         questionsList[1] = "Os jogos olimpicos foram criados para homenagear qual deus grego?";
-        questionsList[2] = "Eparta, a cidade-Estado grega, tinha uma forma de governo em que poucas pessoas participavam. Qual a forma de governo?";
+        questionsList[2] = "Esparta, a cidade-Estado grega, tinha uma forma de governo em que poucas pessoas participavam. Qual a forma de governo?";
         questionsList[3] = "Na mitoligia grega, quem era o deus dos deuses?";
-        questionsList[4] = "Segundo a Mitologia Grega, quem era a deusa da guerra, justiça e sabedoria?";
+        questionsList[4] = "Segundo a Mitologia Grega, quem era a deusa da justica e sabedoria?";
         questionsList[5] = "Como se chamava o filho de Zeus e Alcmena?";
-        questionsList[6] = "Originalmente, quantas cabeças tinha Hidra?";
+        questionsList[6] = "Originalmente, quantas cabecas tinha Hidra?";
         questionsList[7] = "Quem venceu a Guerra do Peloponeso? Atenas ou Esparta?";
         questionsList[8] = "Quem era a deusa do amor?";
-        questionsList[9] = "Hestia era adorada pelos gregos pois representava proteção. Verdadeiro ou Falso?";
+        questionsList[9] = "Hestia era adorada pelos gregos pois representava protecao. Verdadeiro ou Falso?";
     };
     public static String[] answersList = new String[10];
     public static void getAnswersList() {
--- src/quiz.java
+++ src/quiz.java
@@ -19,7 +19,7 @@ public class quiz {
         questions.getQuestionsList();
         questions.getAnswersList();
         Scanner sc = new Scanner(System.in);
-
+  
         //Definido gurreiros disponiveis no game
         String espada =
                 "  0   /\n" +
@@ -32,19 +32,35 @@ public class quiz {
                 " / | \\/\n" +
                 "   |  \n" +
                 "  / \\";
+        
+        
+        String medusa = 
+        		"   ~Õ~\n"
+        		+ "   /|\\   \n"
+        		+ "    |    \n"
+        		+ "   \\|/   \n";
+        
+        String medusaDead = 
+        		"     |}\n"
+        		+ "<-----O}\n"
+        		+ "     |}";
+        
         System.out.println("Selecione entre um dos guerreiros (Lembre-se que quanto maior a ferramenta de ataque, mais lento o ataque especial!)");
         System.out.println("Guerreiro com Espada: \n" + espada + "\n\nGuerreiro com Faca:\n" + faca + "\nEscolha entre guerreiro com espada ou faca?");
         String answer = sc.next().toLowerCase();
 
         String guerreiro = "";
+        int superAttackCounter = 0, correctSequence = 0;
         
         switch (answer){
         	case "faca": {
         		guerreiro = faca;
+        		superAttackCounter = 2;
         		break;
         	}
         	case "espada": {
         		guerreiro = espada;
+        		superAttackCounter = 3;
         		break;
         	}
         	default:{
@@ -54,11 +70,24 @@ public class quiz {
         }
         
         System.out.println("O guerreiro selecionado foi:\n" + guerreiro);
+        System.out.println("Lutando contra a medusa:\n" + medusa);
         
         int fighterLife = 6;
         int monsterLife = 6;
         
         while(fighterLife > 0 && monsterLife > 0) {
+        	
+        	if (correctSequence == superAttackCounter) {
+        		System.out.println("Deseja usar o ataque especial?");
+        		String attackAnswer = sc.next();
+        		
+        		if(attackAnswer.toLowerCase().equals("sim")) {
+        			monsterLife -=2;
+        			System.out.println("Vida do guerreiro: " + fighterLife);
+                    System.out.println("Vida do monstro: " + monsterLife);
+        		}
+        	}
+        	
             double questionNumber = Math.ceil(Math.random()* 10);
         	if (questionNumber > 0) {
         		questionNumber -= 1;
@@ -67,14 +96,13 @@ public class quiz {
             String actualQuestion = questions.questionsList[(int)questionNumber];
             System.out.println(actualQuestion+"\n"
             		+ "-Resposta-> ");
-            String questionAnswer = sc.nextLine();
+            String questionAnswer = sc.next();
             
             if (checkAnswer (questionAnswer, (int)questionNumber)) {
-            	fighterLife += 1;
+            	correctSequence += 1;
             	monsterLife -= 1;
             }
-            if(!checkAnswer (questionAnswer, (int)questionNumber)){            	
-            	monsterLife += 1;
+            if(!checkAnswer (questionAnswer, (int)questionNumber)){
             	fighterLife -= 1;
             }
             
@@ -89,7 +117,7 @@ public class quiz {
         
         if(fighterLife <= 0) System.out.println("Guerreiro morto!");
 
-        if(monsterLife <= 0) System.out.println("Monstro morto!");
+        if(monsterLife <= 0) System.out.println("\n\nMedusa morta!\n" + medusaDead);
         
         sc.close();
     }
